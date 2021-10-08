0445d0cb3732199f2f4719148c5962e677df3a91
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-08 13:16:56 +0300

    poate

--- core/init.php
+++ core/init.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
 
 $GLOBALS['config'] = array(
     "mysql" => array(
