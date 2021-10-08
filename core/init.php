00a300ab2b1df0952045d45fd5357ded3aba72c7
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-08 12:06:55 +0300

    make it better

--- core/init.php
+++ core/init.php
@@ -12,14 +12,14 @@ $cleardb_db = substr($cleardb_url["path"],1);
 
 $GLOBALS['config'] = array(
     "mysql" => array(
-        'host' => $cleardb_server,
-        'username' => $cleardb_username,
-        'password' => $cleardb_password,
-        'db' => $cleardb_db
-        // 'host' => '127.0.0.1',
-        // 'username' => 'root',
-        // 'password' => '',
-        // 'db' => 'oop'
+        // 'host' => $cleardb_server,
+        // 'username' => $cleardb_username,
+        // 'password' => $cleardb_password,
+        // 'db' => $cleardb_db
+        'host' => 'eu-cdbr-west-01.cleardb.com',
+        'username' => 'bd168ab4a8dffd',
+        'password' => 'cd99342f',
+        'db' => 'heroku_2afb099aaeb61b6'
 
     ),
     'remember' => array(
