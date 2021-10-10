0a9e94a34c87f8310696a9d26ebbf9d991d5df40
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-10 21:07:23 +0300

    who knows

--- cos.php
+++ cos.php
@@ -46,14 +46,37 @@
             <?php
 
             if (isset($_GET['send']) && $_GET['send'] == "success") {
-                echo "
+                echo '
                 <script>
-                    alert('Comanda ta a fost plasată. Te rog să aștepți până te voi contacta! ');
-                    let url= new URL(window.location.href);
-                    let params = new URLSearchParams(url.search);
-                    params.delete('send');
+                    alert("Comanda ta a fost plasată. Te rog să aștepți până te voi contacta! ");
+                    // let url= new URL(window.location.href);
+                    // let params = new URLSearchParams(url.search);
+                    // params.delete("send");
+
+                    function removeParam(key, sourceURL) {
+                        var rtn = sourceURL.split("?")[0],
+                            param,
+                            params_arr = [],
+                            queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+                        if (queryString !== "") {
+                            params_arr = queryString.split("&");
+                            for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+                                param = params_arr[i].split("=")[0];
+                                if (param === key) {
+                                    params_arr.splice(i, 1);
+                                }
+                            }
+                            if (params_arr.length) rtn = rtn + "?" + params_arr.join("&");
+                        }
+                        return rtn;
+                    }
+
+
+                    window.location.href = removeParam("send", window.location.href);
+
+
                 </script>
-                ";
+                ';
             }
 
             $cos = new Cos();
