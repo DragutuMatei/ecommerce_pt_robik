5617aecbc08007998260953fa088bf909a2a537c
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-10-29 18:44:30 +0300

    plm

--- admin.php
+++ admin.php
@@ -279,7 +279,7 @@ if (!Session::exists("adminSession")) {
 
     <script>
         const swiper = new Swiper('.swiper-container', {
-            loop: true,
+            loop: false,
 
             pagination: {
                 el: '.swiper-pagination',
