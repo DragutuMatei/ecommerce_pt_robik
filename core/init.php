3ff088b6228fb235a3f38342c8cb889628f998ba
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-03 15:33:52 +0200

    plm

--- core/init.php
+++ core/init.php
@@ -1,22 +1,18 @@
 <?php
 session_start();
 
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"], 1);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
 // mysql://b193caa9e04491:55d7b7bc@eu-cdbr-west-01.cleardb.com/heroku_ffa7e0b75465646?reconnect=true
 $GLOBALS['config'] = array(
     "mysql" => array(
-        // 'host' => $cleardb_server,
-        // 'username' => $cleardb_username,
-        // 'password' => $cleardb_password,
-        // 'db' => $cleardb_db
-        'host' => 'eu-cdbr-west-01.cleardb.com',
-        'username' => 'b193caa9e04491',
-        'password' => '55d7b7bc',
+        'host' => $cleardb_server,
+        'username' => $cleardb_username,
+        'password' => $cleardb_password,
         'db' => 'heroku_ffa7e0b75465646'
 
     ),
