2d6a9cb0faf0988079095dd4571c2e5e618ce6d9
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-03 15:40:06 +0200

    make it better

--- core/init.php
+++ core/init.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+// punktuletz
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
